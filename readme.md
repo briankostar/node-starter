595c23631ff44fb317d030dfc64806191b90842b
Brian Ko <brian.kostar@gmail.com>
2018-10-12 00:15:15 -0400

    add travis

--- readme.md
+++ readme.md
@@ -1,6 +1,16 @@
-# readme
+# Readme
+
+# Purpose
+To be used as bare bone backend for any node applications.
 
 # Usage
 clone this repo
 $ npm install
 $ npm start
+
+# Plans
+Add default routing
+Add database connection
+Add default REST calls
+Add optional 3rd party apis?
+Add linting
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a user resource');
 });
+router.post('/', function (req, res) {
+  res.send('Got a POST request at /users')
+})
+router.put('/', function (req, res) {
+  res.send('Got a PUT request at /users')
+})
+router.delete('/', function (req, res) {
+  res.send('Got a DELETE request at /users')
+})
 
 module.exports = router;
